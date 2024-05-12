8164ccabe3aca746c91d6cabc4faf21ee57b4406
jiejie2023 <140963083+jiejie2023@users.noreply.github.com>
2024-05-13 01:44:52 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -25,9 +25,9 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: '',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '推荐文章', url: '/tag/推荐文章' },
-  HEO_HERO_CATEGORY_2: { title: '学习思考', url: '/tag/阅读思考' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '作者介绍', url: '/about' },
+  HEO_HERO_CATEGORY_2: { title: '推荐文章', url: '/tag/推荐' },
+  HEO_HERO_CATEGORY_3: { title: '学习思考', url: '/tag/学习' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
