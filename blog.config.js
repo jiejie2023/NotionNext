d4ff513cc5814432f9c94ac8c456883b4e4a5ebb
jiejie2023 <140963083+jiejie2023@users.noreply.github.com>
2024-05-17 01:00:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -66,6 +66,7 @@ const BLOG = {
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
     // '"LXGW WenKai"',
+    '"LXGW WenKai"',
     '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',
@@ -86,6 +87,7 @@ const BLOG = {
   // 衬线字体 例如'"LXGW WenKai"'
   FONT_SERIF: [
     // '"LXGW WenKai"',
+    '"LXGW WenKai"',
     'Bitter',
     '"Noto Serif SC"',
     'SimSun',
