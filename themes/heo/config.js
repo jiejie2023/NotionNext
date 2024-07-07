5e08b26328fb1f15409fbc4564e5d6dec97f6b54
jiejie2023 <140963083+jiejie2023@users.noreply.github.com>
2024-07-07 23:04:50 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -47,9 +47,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://jiely.ac.cn/',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://jiely.ac.cn/,
+  HEO_INFO_CARD_URL3: 'https://jiely.ac.cn/category,
   HEO_INFO_CARD_TEXT3: '了解更多',
 
 
