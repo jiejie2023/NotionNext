6dafc6e80f40c7aabdd9fa3c5139e37c5dbb9fb2
jiejie2023 <140963083+jiejie2023@users.noreply.github.com>
2024-07-07 21:48:12 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -108,10 +108,10 @@ HEO_INFO_CARD_URL: 'https://jiely.ac.cn/',
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '联系我🤝',
+  HEO_SOCIAL_CARD_TITLE_2: '向上社交，随时欢迎！',
+  HEO_SOCIAL_CARD_TITLE_3: '点击添加我的wechat📩',
+  HEO_SOCIAL_CARD_URL: 'https://jiely.ac.cn/about',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
