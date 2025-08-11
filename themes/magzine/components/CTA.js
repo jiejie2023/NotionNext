7656bdfddab55241f4cdb840eb1c9d585ab202cf
jiejie2023 <140963083+jiejie2023@users.noreply.github.com>
2025-08-11 22:05:32 +0800

    Update CTA.js

--- themes/magzine/components/CTA.js
+++ themes/magzine/components/CTA.js
@@ -12,7 +12,7 @@ export default function CTA({ notice }) {
       <Announcement
         post={notice}
         className={
-          'cta text-center text-black bg-[#DCE2F1 ] dark:bg-hexo-black-gray py-16'
+          'cta text-center text-black bg-[#DCE2F1] dark:bg-hexo-black-gray py-16'
         }
       />
     </>
